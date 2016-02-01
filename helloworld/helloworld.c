9d9fbba773048bb84c3da9d4f702508ee43b9247
Tiziano Cappellari <tcappellari@nabertech.com>
2016-02-01 10:08:30 +0100

    Fix bye bye message

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -2,5 +2,5 @@
 
 void main() {	// inizio blocco di codice funzione main => main === inizio programma
 	printf("Hello world!\n\n");
-	printf("Bye bye");
+	printf("Bye bye\n");
 } // fine blocco di codice funzione main
